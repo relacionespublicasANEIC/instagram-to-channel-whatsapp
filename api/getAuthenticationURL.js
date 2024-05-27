b797633e8627eac627a2ad1de8dd334b59e31862
Secretaria de relaciones públicas de ANEIC Colombia <secrelapublicasaneiccolombia@gmail.com>
2024-05-27 01:35:07 -0500

    Update getAuthenticationURL.js

--- api/getAuthenticationURL.js
+++ api/getAuthenticationURL.js
@@ -3,7 +3,7 @@ const { INSTAGRAM_CLIENT_ID, BASE_URL } = process.env;
 export async function GET() {
     return new Response(createURLWithParams("https://api.instagram.com/oauth/authorize", {
         client_id: INSTAGRAM_CLIENT_ID,
-        redirect_uri: `https://${BASE_URL}/api/v1/auth`,
+        redirect_uri: `https://${BASE_URL}/api/auth`,
         scope: "user_profile,user_media",
         response_type: "code"
     }));
